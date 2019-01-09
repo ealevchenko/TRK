747d0f548bf76b9822275406f49000e14e438489
Levchenko <lev75.eduard@gmail.com>
2019-01-09 23:33:35 +0200

    fix error and add algoritm read rfid

--- ClientOPCTRK/ClientTRK.cs
+++ ClientOPCTRK/ClientTRK.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessageLog;
 
 namespace ClientOPCTRK
 {
@@ -96,50 +97,56 @@ namespace ClientOPCTRK
 
         public void AddGun(ref Opc.Da.Item[] items, ref int i, int num_trk, int num_gun)
         {
-
-            //i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".current_volume";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".density";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".last_out_volume";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".online";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".passage";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".price_to_write";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".start";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".state";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".stop";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".taken";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".total_volume";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".Trk0" + num_trk + "_" + num_gun + "_status";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".volume_to_write";
-            i++;
-            items[i] = new Opc.Da.Item();
-            items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".write_price";
-            i++;
+            try
+            {
+                //i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".current_volume";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".density";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".last_out_volume";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".online";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".passage";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".price_to_write";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".start";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".state";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".stop";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".taken";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".total_volume";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".Trk0" + num_trk + "_" + num_gun + "_status";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".volume_to_write";
+                i++;
+                items[i] = new Opc.Da.Item();
+                items[i].ItemName = "TRK.Trk0" + num_trk + "_" + num_gun + ".write_price";
+                i++;
+            }
+            catch (Exception e)
+            {
+                String.Format("Ошибка выполнения метода AddGun(items={0}, i={1}, num_trk={2}, num_gun={3})", items, i, num_trk, num_gun).SaveError(e);
+            }
         }
 
         /// <summary>
@@ -154,42 +161,113 @@ namespace ClientOPCTRK
             {
                 EFazsCards ef_card = new EFazsCards();
                 EFazsDeparts ef_departs = new EFazsDeparts();
-                if (hi != null && lo != null && (uint)hi>0  && (uint)lo>0)
+                if (hi != null && lo != null && (uint)hi > 0 && (uint)lo > 0)
                 {
                     //Console.WriteLine("hi={0}, lo={1}",hi,lo);
-                    azsCards card = ef_card.Get().Where(c => c.Number == ((uint)hi).ToString("000") + "," + ((uint)lo).ToString("00000")).FirstOrDefault();
-                    azsDeparts departs = null;
-                    if (card != null)
+                    byte[] data_hi = BitConverter.GetBytes((uint)hi);
+                    byte[] data_lo = BitConverter.GetBytes((uint)lo);
+
+                    if (data_hi != null && data_lo != null && data_hi.Count() >= 2 && data_lo.Count() >= 2)
                     {
-                        departs = ef_departs.Get().Where(d => d.id == ((int)card.House).ToString("000")).FirstOrDefault();
+                        int code1 = data_hi[1];
+                        int code2 = (data_hi[0] * 256) + data_lo[1];
+                        azsCards card = ef_card.Get().Where(c => c.Number == (code1).ToString("000") + "," + (code2).ToString("00000")).FirstOrDefault();
+                        azsDeparts departs = null;
+                        if (card != null)
+                        {
+                            departs = ef_departs.Get().Where(d => d.id == ((int)card.House).ToString("000")).FirstOrDefault();
+                        }
+                        Cards cards = new Cards()
+                        {
+                            Id = card.Id,
+                            Number = card.Number,
+                            DriverName = card.DriverName,
+                            AutoNumber = card.AutoNumber,
+                            Debitor = card.Debitor,
+                            Sn1 = card.Sn1,
+                            Sn2 = card.Sn2,
+                            AutoModel = card.AutoModel,
+                            Street = card.Street,
+                            House = card.House,
+                            CreateDate = card.CreateDate,
+                            CreateTime = card.CreateTime,
+                            UpdateDate = card.UpdateDate,
+                            UpdateTime = card.UpdateTime,
+                            Owner = card.Owner,
+                            Active = card.Active,
+                            Name = departs != null && departs.name != null ? departs.name : "?",
+                        };
+                        return cards;
                     }
-                    Cards cards = new Cards()
-                    {
-                        Id = card.Id,
-                        Number = card.Number,
-                        DriverName = card.DriverName,
-                        AutoNumber = card.AutoNumber,
-                        Debitor = card.Debitor,
-                        Sn1 = card.Sn1,
-                        Sn2 = card.Sn2,
-                        AutoModel = card.AutoModel,
-                        Street = card.Street,
-                        House = card.House,
-                        CreateDate = card.CreateDate,
-                        CreateTime = card.CreateTime,
-                        UpdateDate = card.UpdateDate,
-                        UpdateTime = card.UpdateTime,
-                        Owner = card.Owner,
-                        Active = card.Active,
-                        Name = departs!= null && departs.name != null ? departs.name : "?",
-                    };
-                    return cards;
                 }
                 return null;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода GetCardsOfNum(hi={0}, lo={1})", hi, lo).SaveError(e);
+                return null;
+            }
+        }
+
+        private RFID GetRFIDOfNum(int num_trk, int side, object hi, object lo)
+        {
+            try
+            {
+                RFID rfid = new RFID()
+                {
+                     num_trk= num_trk, side = side
+                };
+
+                EFazsCards ef_card = new EFazsCards();
+                EFazsDeparts ef_departs = new EFazsDeparts();
+                if (hi != null && lo != null && (uint)hi > 0 && (uint)lo > 0)
+                {
+                    rfid.hi = (uint)hi;
+                    rfid.lo = (uint)lo;
+                    //Console.WriteLine("hi={0}, lo={1}",hi,lo);
+                    byte[] data_hi = BitConverter.GetBytes((uint)hi);
+                    byte[] data_lo = BitConverter.GetBytes((uint)lo);
+
+                    if (data_hi != null && data_lo != null && data_hi.Count() >= 2 && data_lo.Count() >= 2)
+                    {
+                        int code1 = data_hi[1];
+                        int code2 = (data_hi[0] * 256) + data_lo[1];
+                        rfid.hi = (uint)code1;
+                        rfid.lo = (uint)code2;
+                        azsCards card = ef_card.Get().Where(c => c.Number == (code1).ToString("000") + "," + (code2).ToString("00000")).FirstOrDefault();
+                        azsDeparts departs = null;
+                        if (card != null)
+                        {
+                            departs = ef_departs.Get().Where(d => d.id == ((int)card.House).ToString("000")).FirstOrDefault();
+                        }
+                        Cards cards = new Cards()
+                        {
+                            Id = card.Id,
+                            Number = card.Number,
+                            DriverName = card.DriverName,
+                            AutoNumber = card.AutoNumber,
+                            Debitor = card.Debitor,
+                            Sn1 = card.Sn1,
+                            Sn2 = card.Sn2,
+                            AutoModel = card.AutoModel,
+                            Street = card.Street,
+                            House = card.House,
+                            CreateDate = card.CreateDate,
+                            CreateTime = card.CreateTime,
+                            UpdateDate = card.UpdateDate,
+                            UpdateTime = card.UpdateTime,
+                            Owner = card.Owner,
+                            Active = card.Active,
+                            Name = departs != null && departs.name != null ? departs.name : "?",
+                        };
+                        rfid.card = cards;
+                    }
+                }
+                return rfid;
+            }
+            catch (Exception e)
+            {
+                String.Format("Ошибка выполнения метода GetRFIDOfNum(num_trk={0}, side={1}, hi={2}, lo={3})", num_trk, side, hi, lo).SaveError(e);
                 return null;
             }
         }
@@ -276,7 +354,7 @@ namespace ClientOPCTRK
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода GetGun(num_trk={0}, num_gun={1}, site={2}, list={3}, start={4})", num_trk, num_gun, site, list, start).SaveError(e);
                 return null;
             }
 
@@ -438,7 +516,7 @@ namespace ClientOPCTRK
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода ReadTagOPC()").SaveError(e);
                 return null;
             }
         }
@@ -521,7 +599,7 @@ namespace ClientOPCTRK
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода ReadTagsOPSOfTank(num_tank={0})", num_tank).SaveError(e);
                 return null;
             }
         }
@@ -631,34 +709,43 @@ namespace ClientOPCTRK
                 items[32].ItemName = "RFID.Rfid09_1.IdHi";
                 items[33] = new Opc.Da.Item();
                 items[33].ItemName = "RFID.Rfid09_1.IdLo";
-                //int i = 33;
-                //AddGun(ref items, ref i, 6, 0);
+
                 items = group.AddItems(items);
                 List<RFID> result_list = new List<RFID>();
                 ItemValueResult[] res = group.Read(items);
-                result_list.Add(new RFID { num_trk = 1, side = 0, card = GetCardsOfNum(res[0].Value, res[1].Value), hi = res[0].Value != null ? res[0].Value as uint? : null, lo = res[1].Value != null ? res[1].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 1, side = 1, card = GetCardsOfNum(res[2].Value, res[3].Value), hi = res[2].Value != null ? res[2].Value as uint? : null, lo = res[3].Value != null ? res[3].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 2, side = 0, card = GetCardsOfNum(res[4].Value, res[5].Value), hi = res[4].Value != null ? res[4].Value as uint? : null, lo = res[5].Value != null ? res[5].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 2, side = 1, card = GetCardsOfNum(res[6].Value, res[7].Value), hi = res[6].Value != null ? res[6].Value as uint? : null, lo = res[7].Value != null ? res[7].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 3, side = 0, card = GetCardsOfNum(res[8].Value, res[9].Value), hi = res[8].Value != null ? res[8].Value as uint? : null, lo = res[9].Value != null ? res[9].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 3, side = 1, card = GetCardsOfNum(res[10].Value, res[11].Value), hi = res[10].Value != null ? res[10].Value as uint? : null, lo = res[11].Value != null ? res[11].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 4, side = 0, card = GetCardsOfNum(res[12].Value, res[13].Value), hi = res[12].Value != null ? res[12].Value as uint? : null, lo = res[13].Value != null ? res[13].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 4, side = 1, card = GetCardsOfNum(res[14].Value, res[15].Value), hi = res[14].Value != null ? res[14].Value as uint? : null, lo = res[15].Value != null ? res[15].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 5, side = 0, card = GetCardsOfNum(res[16].Value, res[17].Value), hi = res[16].Value != null ? res[16].Value as uint? : null, lo = res[17].Value != null ? res[17].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 5, side = 1, card = GetCardsOfNum(res[18].Value, res[19].Value), hi = res[18].Value != null ? res[18].Value as uint? : null, lo = res[19].Value != null ? res[19].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 6, side = 0, card = GetCardsOfNum(res[20].Value, res[21].Value), hi = res[20].Value != null ? res[20].Value as uint? : null, lo = res[21].Value != null ? res[21].Value as uint? : null});
-                //result_list.Add(new RFID { num_trk = 6, side = 0, card = GetCardsOfNum((uint)37, (uint)50907), hi = 37, lo = 50907 });
-                result_list.Add(new RFID { num_trk = 6, side = 1, card = GetCardsOfNum(res[22].Value, res[23].Value), hi = res[22].Value != null ? res[22].Value as uint? : null, lo = res[23].Value != null ? res[23].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 7, side = 0, card = GetCardsOfNum(res[24].Value, res[25].Value), hi = res[24].Value != null ? res[24].Value as uint? : null, lo = res[25].Value != null ? res[25].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 7, side = 1, card = GetCardsOfNum(res[26].Value, res[27].Value), hi = res[26].Value != null ? res[26].Value as uint? : null, lo = res[27].Value != null ? res[27].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 8, side = 0, card = GetCardsOfNum(res[28].Value, res[29].Value), hi = res[28].Value != null ? res[28].Value as uint? : null, lo = res[29].Value != null ? res[29].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 8, side = 1, card = GetCardsOfNum(res[30].Value, res[31].Value), hi = res[30].Value != null ? res[30].Value as uint? : null, lo = res[31].Value != null ? res[31].Value as uint? : null });
-                result_list.Add(new RFID { num_trk = 9, side = 0, card = GetCardsOfNum(res[32].Value, res[33].Value), hi = res[32].Value != null ? res[32].Value as uint? : null, lo = res[33].Value != null ? res[33].Value as uint? : null });
+
+                result_list.Add(GetRFIDOfNum(1, 0, res[0].Value, res[1].Value));
+                result_list.Add(GetRFIDOfNum(1, 1, res[2].Value, res[3].Value));
+
+                result_list.Add(GetRFIDOfNum(2, 0, res[4].Value, res[5].Value));
+                result_list.Add(GetRFIDOfNum(2, 1, res[6].Value, res[7].Value));
+
+                result_list.Add(GetRFIDOfNum(3, 0, res[8].Value, res[9].Value));
+                result_list.Add(GetRFIDOfNum(3, 1, res[10].Value, res[11].Value));
+
+                result_list.Add(GetRFIDOfNum(4, 0, res[12].Value, res[13].Value));
+                result_list.Add(GetRFIDOfNum(4, 1, res[14].Value, res[15].Value));
+
+                result_list.Add(GetRFIDOfNum(5, 0, res[16].Value, res[17].Value));
+                result_list.Add(GetRFIDOfNum(5, 1, res[18].Value, res[19].Value));
+
+                result_list.Add(GetRFIDOfNum(6, 0, res[20].Value, res[21].Value));
+                //result_list.Add(GetRFIDOfNum(6, 0, (uint)10006, (uint)54273));
+                result_list.Add(GetRFIDOfNum(6, 1, res[22].Value, res[23].Value));
+
+                result_list.Add(GetRFIDOfNum(7, 0, res[24].Value, res[25].Value));
+                result_list.Add(GetRFIDOfNum(7, 1, res[26].Value, res[27].Value));
+
+                result_list.Add(GetRFIDOfNum(8, 0, res[28].Value, res[29].Value));
+                result_list.Add(GetRFIDOfNum(8, 1, res[30].Value, res[31].Value));
+
+                result_list.Add(GetRFIDOfNum(9, 0, res[32].Value, res[33].Value));
+
                 return result_list;
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода ReadTagsOPSOfRFID()").SaveError(e);
                 return null;
             }
         }
@@ -704,7 +791,7 @@ namespace ClientOPCTRK
             }
             catch (Exception e)
             {
-                Console.WriteLine(e);
+                String.Format("Ошибка выполнения метода ReadTagOPCOfGun()").SaveError(e);
                 return null;
             }
         }
